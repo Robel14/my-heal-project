98a5c402b582e92ee702e1d818506e9d9e6ea527
Robel14 <104377856+Robel14@users.noreply.github.com>
2025-06-28 16:42:54 +0600

    Update page.tsx

--- app/about/team/page.tsx
+++ app/about/team/page.tsx
@@ -14,7 +14,7 @@ const teamMembers = [
     name: "Dereje Deme",
     position: "Executive Director",
     bio: "Brings over two decades of experience in public and private institutions. He holds a BA in Organizational Management and a Diploma in Agriculture and oversees daily operations, external relations, and fundraising endeavors of HEAL.",
-    image: "/placeholder.svg?height=300&width=300",
+    image: "photo_2025-06-28_13-38-21.jpg",
   },
 ]
 
