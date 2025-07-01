83ed01505e1416f54dc512c8bf4b8a3302e61a0a
Robel14 <104377856+Robel14@users.noreply.github.com>
2025-07-02 02:29:29 +0600

    Create route.ts

---
+++ app/api/create-video/route.ts
@@ -0,0 +1,17 @@
+import { createClient } from '@supabase/supabase-js';
+import { NextRequest, NextResponse } from 'next/server';
+
+const supabase = createClient('https://ibupkheuaufgdgxngtwl.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlidXBraGV1YXVmZ2RneG5ndHdsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTEzOTAwNzIsImV4cCI6MjA2Njk2NjA3Mn0.C65DWGzuI23sCwTt2xrnWHlqfnQg4Mzt7ON5KqXgXyM');
+
+export async function POST(request: NextRequest) {
+  const body = await request.json();
+  const { title, video_url } = body;
+
+  const { data, error } = await supabase.from('videos').insert([
+    { title, video_url },
+  ]);
+
+  if (error) return NextResponse.json({ error: error.message }, { status: 500 });
+
+  return NextResponse.json({ message: 'Video added', data });
+}
